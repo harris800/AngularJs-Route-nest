1871a47fa4d8a6f9b415b0cac8a23cc02994e929
harris800 <harris_800@hotmail.com>
2015-04-14 17:11:38 -0400

    uncommand the config

--- app/app.js
+++ app/app.js
@@ -3,12 +3,12 @@
  */
 var myApp = angular.module("myApp",["ui-router"]);
 
-//myApp.config(function ($stateProvider, $urlRouterProvider){
-//    $urlRouterProvider.when("", "/PageTab/Page1");
-//    $stateProvider
-//        .state("PageTab", { url: "/PageTab", templateUrl: "app/partials/PageTab.html" })
-//        .state("PageTab.Page1", { url: "app/partials/Page1", templateUrl: "Page1.html" })
-//        .state("PageTab.Page2", { url: "app/partials/Page2", templateUrl: "Page2.html" })
-//        .state("PageTab.Page3", { url: "app/partials/Page3", templateUrl: "Page3.html" });
-//
-//});
+myApp.config(function ($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.when("", "/PageTab/Page1");
+    $stateProvider
+        .state("PageTab", { url: "/PageTab", templateUrl: "app/partials/PageTab.html" })
+        .state("PageTab.Page1", { url: "app/partials/Page1", templateUrl: "Page1.html" })
+        .state("PageTab.Page2", { url: "app/partials/Page2", templateUrl: "Page2.html" })
+        .state("PageTab.Page3", { url: "app/partials/Page3", templateUrl: "Page3.html" });
+
+});
