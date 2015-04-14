e991df75041ed459c05e71e3ebffa91c2b32c34d
harris800 <harris_800@hotmail.com>
2015-04-14 17:07:14 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Yuan on 4/14/2015.
+ */
+var myApp = angular.module("myApp",["ui-router"]);
+
+//myApp.config(function ($stateProvider, $urlRouterProvider){
+//    $urlRouterProvider.when("", "/PageTab/Page1");
+//    $stateProvider
+//        .state("PageTab", { url: "/PageTab", templateUrl: "app/partials/PageTab.html" })
+//        .state("PageTab.Page1", { url: "app/partials/Page1", templateUrl: "Page1.html" })
+//        .state("PageTab.Page2", { url: "app/partials/Page2", templateUrl: "Page2.html" })
+//        .state("PageTab.Page3", { url: "app/partials/Page3", templateUrl: "Page3.html" });
+//
+//});
